aeee804cd2eb2f9ca4554d88be01758200755d79
Ganzsoo <lacertaev@gmail.com>
2024-10-28 20:42:00 +0100

    todo lo de android

--- INTERFACES/Tema1/Proyecto/Conversor.py
+++ INTERFACES/Tema1/Proyecto/Conversor.py
@@ -1,7 +1,9 @@
 import sys
 import os
 from PyQt6 import QtWidgets, uic
-from PyQt6.QtGui import QIntValidator
+from PyQt6.QtGui import QIntValidator,QFont
+from PyQt6.QtCore import QTimer
+from datetime import datetime
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 Conversorui_file_path = os.path.join(script_dir, "Conversor.ui")
@@ -12,7 +14,7 @@ class Ventana(QtWidgets.QMainWindow):
     def __init__(self, padre=None):
         QtWidgets.QMainWindow.__init__(self, padre)
         uic.loadUi(Conversorui_file_path, self)
-        self.setWindowTitle("Ejemplo")
+        self.setWindowTitle("Conversor de Monedas")
         
 
         self.cerrarVentana.clicked.connect(self.close)
@@ -29,16 +31,36 @@ class Ventana(QtWidgets.QMainWindow):
         self.monedaEntrante.currentIndexChanged.connect(self.actualizar_conversion)
         self.monedaSaliente.currentIndexChanged.connect(self.actualizar_conversion)
 
-        self.aniadir.clicked.connect(self.abrirVentana)
+        self.btn_aniadir.clicked.connect(self.abrirVentana)
 
         self.moneda = [1.0, 0.85, 110.0, 0.75, 0.94]  # USD, EUR, JPY, GBP, CHF
         self.simbolo = ["$", "€", "¥", "£", "₣"]      # USD, EUR, JPY, GBP, CHF
         self.cargarMoneda()
 
-    def cargarMoneda(self):
+        fuente = QFont()
+        fuente.setPointSize(34) 
+        self.hora.setFont(fuente)
+        
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.actualizar_hora)
+        self.timer.start(1000) 
+
+        
+        self.actualizar_hora()
+
+    def actualizar_hora(self):
        
-       try:
-         with open(monedas_txt, 'r') as file:
+        hora_actual = datetime.now().strftime("%H:%M")
+        self.hora.setText(hora_actual)  
+
+    def cargarMoneda(self):
+      
+      if not os.path.exists(monedas_txt):
+        with open(monedas_txt, 'w') as file:
+            pass 
+       
+      try:
+          with open(monedas_txt, 'r') as file:
             for line in file:
                 nombre, simbolo, valor = line.strip().split(',')  
                 valor = float(valor) 
@@ -47,8 +69,8 @@ class Ventana(QtWidgets.QMainWindow):
                 self.monedaEntrante.addItem(nombre+" ("+nombre[:3].upper()+")")
                 self.monedaSaliente.addItem(nombre+" ("+nombre[:3].upper()+")")
                
-       except FileNotFoundError:
-        print("El archivo 'moneda.txt' no existe.")
+      except FileNotFoundError:
+           print("El archivo 'moneda.txt' no existe.")
 
     def ValorMoneda1(self, index):
         self.moneda1 = self.moneda[index]
@@ -71,47 +93,34 @@ class Ventana(QtWidgets.QMainWindow):
         else:
             self.label.clear()
 
-    #TODO: ELIMINAR_MONEDA
-    def eliminar_moneda(self, index):
-   
-     if 0 <= index < len(self.moneda):
-       
-        del self.moneda[index]
-        del self.simbolo[index]
 
-        
-        self.monedaEntrante.removeItem(index)
-        self.monedaSaliente.removeItem(index)
-
-        QtWidgets.QMessageBox.information(self, "Éxito", "Moneda eliminada con éxito.")
-     else:
-        QtWidgets.QMessageBox.warning(self, "Error", "Índice no válido para eliminar la moneda.")        
 
     def abrirVentana(self):
         if self.ventana_secundaria is None:
             self.ventana_secundaria = AgregarMoneda(self)  
         self.ventana_secundaria.show()
 
-    def anadir_moneda(self, nombre, simbolo, valor): 
-      # Comprobar si la moneda ya existe
+    def verificar_moneda(self, nombre, simbolo, valor): 
+     
         with open(monedas_txt, 'r') as file:
+          
           for line in file:
             nombreTxt, simboloTxt, valorTxt = line.strip().split(',')
 
-            if nombreTxt.upper() == nombre.upper():  # Comparación correcta
+            if nombreTxt.upper() == nombre.upper(): 
                 alerta = QtWidgets.QMessageBox()
-                alerta.setText("Esa moneda ya existe")  # Texto del mensaje
-                alerta.setWindowTitle("Advertencia")  # Título de la ventana
-                alerta.setStandardButtons(QtWidgets.QMessageBox.StandardButton.Ok)  # Botón "Aceptar"
-                alerta.exec()  # Mostrar la ventana de alerta
-                return  # Salir de la función si la moneda ya existe
+                alerta.setText("Esa moneda ya existe")  
+                alerta.setWindowTitle("Advertencia")  
+                alerta.setStandardButtons(QtWidgets.QMessageBox.StandardButton.Ok) 
+                alerta.exec()  
+                return  
+            
+        self.aniadir(nombre, simbolo, valor)
 
-              # Si no existe, agregar la nueva moneda
-          with open(monedas_txt, 'a') as file:  
-             file.write(f"{nombre},{simbolo},{valor}\n") 
-             self.cargarMoneda()  # Cargar las monedas nuevamente
-
-        
+    def aniadir(self, nombre, simbolo, valor):
+          with open(monedas_txt, 'a') as file:
+              file.write(f"{nombre},{simbolo},{valor}\n") 
+          self.cargarMoneda()
         
 
 class AgregarMoneda(QtWidgets.QDialog):
@@ -119,10 +128,12 @@ class AgregarMoneda(QtWidgets.QDialog):
         super(AgregarMoneda, self).__init__(parent)
         uic.loadUi(añadirMonedaui_file_path, self)
 
+        self.introducirValor.setValidator(QIntValidator())
         self.cerrar.clicked.connect(self.close)
         self.agregar.clicked.connect(self.enviar_datos) 
 
     def enviar_datos(self):
+       
        
         nombre = self.introducirNom.text().strip()
         simbolo = self.introducirSim.text().strip()
@@ -131,13 +142,19 @@ class AgregarMoneda(QtWidgets.QDialog):
         except ValueError:
             valor = None
 
-        if not nombre or not simbolo or valor is None:
+        if not nombre or valor is None:
             QtWidgets.QMessageBox.warning(self, "Error", "Por favor, completa todos los campos correctamente.")
             return
-
+        
         parent = self.parent()
-        if isinstance(parent, Ventana):  
-            parent.anadir_moneda(nombre, simbolo, valor)  
+        if isinstance(parent, Ventana):
+          if simbolo is None:
+             simbolo = nombre[:3].upper()  
+          parent.verificar_moneda(nombre, simbolo, valor)  
+          
+        self.introducirNom.clear()
+        self.introducirSim.clear()
+        self.introducirValor.clear()
         self.accept()  
 
 
