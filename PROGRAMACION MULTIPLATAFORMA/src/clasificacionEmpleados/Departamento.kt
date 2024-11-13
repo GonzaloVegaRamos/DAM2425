7a99179aeb20472dd38846717b7434ac486bcd00
Ganzsoo <lacertaev@gmail.com>
2024-11-13 19:42:20 +0100

    limpieza

--- PROGRAMACION MULTIPLATAFORMA/src/clasificacionEmpleados/Departamento.kt
+++
@@ -1,8 +0,0 @@
-package clasificacionEmpleados
-
-// Definimos la enum class para los departamentos
-enum class Departamento {
-    ADMINISTRACION,
-    VENTAS,
-    PRODUCCION
-}
--- PROGRAMACION MULTIPLATAFORMA/src/clasificacionEmpleados/Empleado.kt
+++
@@ -1,8 +0,0 @@
-package clasificacionEmpleados
-
-// Definimos la data class Empleado
-data class Empleado(
-    val nombre: String,
-    val edad: Int,
-    val departamento: Departamento
-)
